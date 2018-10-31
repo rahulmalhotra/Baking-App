a185edbd14e6c9b499bc9a5674c2ff2f293b11c7
rahulmalhotra <rahulmalhotra9620@yahoo.in>
2018-10-31 10:52:58 +0530

    MainActivity (RecipeList) completed and configured for phone and android. Started working on RecipeDetail Activity

--- app/src/main/java/com/example/rahulmalhotra/bakingapp/Adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/rahulmalhotra/bakingapp/Adapters/RecipeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.rahulmalhotra.bakingapp.Adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,12 +9,24 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.rahulmalhotra.bakingapp.MainActivity;
+import com.example.rahulmalhotra.bakingapp.Objects.Recipe;
 import com.example.rahulmalhotra.bakingapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    private List<Recipe> recipeList;
+
+    public void setRecipeList(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+        this.notifyDataSetChanged();
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -23,11 +36,20 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        Recipe recipe = recipeList.get(holder.getLayoutPosition());
+        holder.recipeTitle.setText(recipe.getName());
+        holder.recipeServing.setText("Servings: " + String.valueOf(recipe.getServings()));
+        if(!recipe.getImage().isEmpty()) {
+            Picasso.get().load(recipe.getImage()).into(holder.recipeImage);
+        } else {
+            Picasso.get().load(R.drawable.recipeicon).into(holder.recipeImage);
+        }
     }
 
     @Override
     public int getItemCount() {
+        if(recipeList!=null)
+            return recipeList.size();
         return 0;
     }
 
@@ -41,7 +63,7 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ButterKnife.bind(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
--- app/src/main/java/com/example/rahulmalhotra/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/rahulmalhotra/bakingapp/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.rahulmalhotra.bakingapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.rahulmalhotra.bakingapp.API.BakingAPIService;
 import com.example.rahulmalhotra.bakingapp.API.RetrofitClient;
+import com.example.rahulmalhotra.bakingapp.Adapters.RecipeAdapter;
 import com.example.rahulmalhotra.bakingapp.Objects.Recipe;
 
 import java.util.List;
@@ -22,26 +31,63 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recipeList)
     public RecyclerView recipeListRecyclerView;
     private String TAG = MainActivity.class.getSimpleName();
+    private boolean isTablet;
+    private Integer recyclerViewColumns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        Log.d("test", String.valueOf(recipeListRecyclerView));
-        BakingAPIService bakingAPIService = RetrofitClient.getClient("https://d17h27t6h515a5.cloudfront.net/").create(BakingAPIService.class);
-        bakingAPIService.getRecipes().enqueue(new Callback<List<Recipe>>() {
-            @Override
-            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-                if(response.isSuccessful()) {
-                    Log.d( TAG, response.body().toString());
-                }
-            }
 
-            @Override
-            public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                Log.d( TAG, t.getMessage());
+        // Setting layout according to screen orientation and size
+        isTablet = getResources().getBoolean(R.bool.isTablet);
+        Integer screenOrientation = this.getResources().getConfiguration().orientation;
+        if(isTablet) {
+            if(screenOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+                recyclerViewColumns = 2;
+            } else {
+                recyclerViewColumns = 3;
             }
-        });
+        } else {
+            if(screenOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+                recyclerViewColumns = 1;
+            } else {
+                recyclerViewColumns = 2;
+            }
+        }
+
+        // Making Api Callout
+        final RecipeAdapter adapter = new RecipeAdapter();
+        if(isNetworkAvailable()) {
+            BakingAPIService bakingAPIService = RetrofitClient.getClient("https://d17h27t6h515a5.cloudfront.net/").create(BakingAPIService.class);
+            bakingAPIService.getRecipes().enqueue(new Callback<List<Recipe>>() {
+                @Override
+                public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                    if(response.isSuccessful()) {
+                        adapter.setRecipeList(response.body());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                    Log.d( TAG, t.getMessage());
+                }
+            });
+        } else {
+            Toast.makeText(this, "Device is not connected to internet", Toast.LENGTH_SHORT).show();
+        }
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, recyclerViewColumns);
+        recipeListRecyclerView.setLayoutManager(layoutManager);
+        recipeListRecyclerView.setHasFixedSize(true);
+        recipeListRecyclerView.setAdapter(adapter);
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo!=null && networkInfo.isConnected();
     }
 }
